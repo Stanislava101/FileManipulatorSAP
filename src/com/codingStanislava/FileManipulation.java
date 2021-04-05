76ad2ef28b0f512a1c03b4b8c2c3ac608de71242
Stanislava Georgieva <stanislava.krasimirovaa@abv.bg>
2021-04-05 17:06:55 +0300

    Code optimization

--- src/com/codingStanislava/FileManipulation.java
+++ src/com/codingStanislava/FileManipulation.java
@@ -16,11 +16,6 @@ public class FileManipulation {
            System.out.println("Text added to file");
            fw.close();
     }
-    public void writeToFileNewText(List<String> text) throws IOException {
-        FileWriter fw = new FileWriter(path);
-        fw.write(String.valueOf(text));
-        fw.close();
-    }
 
     public void readFromFile(String filepath) throws FileNotFoundException {
         try
@@ -153,84 +148,4 @@ public class FileManipulation {
         }
         fw.close();
     }
-  /*  public void switchLines2(int line1, int line2) throws IOException {
-        final List<String> text = Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8);
-        List<String> finalText = new ArrayList<String>();
-        List<String> newStr = new ArrayList<String>();
-        List<String> newStr2 = new ArrayList<>();
-        List<String> mString = new ArrayList<String>();
-        List<String> mString2 = new ArrayList<String>();
-        String newLine1 = "";
-        String newLine2 = "";
-        String str="";
-        String str2="";
-        String replacedWords, replacedWords2 = "";
-        if (text.size() == 0) {
-            System.out.println("File is empty");
-            GUI.text.setText("File is empty");
-        }
-        if(line1<line2) {
-            newLine1 = String.valueOf(text.get(line1));
-            for (int j = 0; j < line1; j++) {
-                newStr2.add(text.get(j));
-            }
-
-            newLine2 = String.valueOf(text.get(line2));
-
-            System.out.println("Line 1 = " + newLine1);
-            System.out.println("Line 2 = " + newLine2);
-            for (int k = line1 + 1; k < line2; k++) {
-                mString.add(text.get(k));
-            }
-            replacedWords = newLine1.replace(newLine1, newLine2);
-            replacedWords2 = newLine2.replace(newLine2, newLine1);
-            newStr.add(replacedWords);
-            newStr.addAll(mString);
-            newStr.add(replacedWords2);
-            int line3 = line2 + 1;
-            List<String> subListData = text.subList(line3, text.size());
-
-            finalText.addAll(newStr2);
-            finalText.addAll(newStr);
-            finalText.addAll(subListData);
-            for (String t : finalText) {
-                System.out.println(t);
-            }
-
-        }else if(line1>line2){
-
-            newLine1 = String.valueOf(text.get(line1));
-            for (int j = 0; j < line2; j++) {
-                mString2.add(text.get(j));
-
-            }
-
-            newLine2 = String.valueOf(text.get(line2));
-
-            System.out.println("Line 1 = " + str);
-            System.out.println("Line 2 = " + str2);
-            for (int q = line2+1; q < line1; q++) {
-                mString.add(text.get(q));
-            }
-            replacedWords = newLine1.replace(newLine1, newLine2);
-            replacedWords2 = newLine2.replace(newLine2, newLine1);
-            newStr.add(replacedWords2);
-            newStr.addAll(mString);
-            newStr.add(replacedWords);
-            List<String> subListData = text.subList(line1+1, text.size());
-
-            finalText.addAll(mString2);
-            finalText.addAll(newStr);
-            finalText.addAll(subListData);
-            for (String t : finalText) {
-                System.out.println(t);
-            }
-        }
-        FileWriter fw = new FileWriter(path);
-        for(String t: finalText){
-            fw.write(t + System.lineSeparator());
-        }
-        fw.close();
-    }
-   */
 }
